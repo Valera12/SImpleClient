68b173dba4a1bde58029d1b857dc1e33c47d8403
Valera Tikavy <whiteleone6@gmail.com>
2018-02-21 16:53:50 +0300

    it's finaly work!!

--- src/SocketTest.java
+++ src/SocketTest.java
@@ -6,15 +6,26 @@ public class SocketTest {
 
     public static void main(String[] args) {
         try{
+
         Socket socket = new Socket("localhost", 8189);
-        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        boolean more = true;
-        while (more){
-            String line = in.readLine();
-            if(line == null)
-                more = false;
-            else System.out.println(line);
-        }
+
+        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        PrintWriter outToServer = new PrintWriter(socket.getOutputStream(), true);
+        String msgFromUser;
+            System.out.println("Enter messages to send:");
+            while((msgFromUser = inFromUser.readLine()) != null){
+                outToServer.println(msgFromUser);
+                String msgFromServer = inFromServer.readLine();
+                System.out.println(msgFromServer);
+            }
+
+                inFromServer.close();
+                inFromUser.close();
+                outToServer.close();
+                socket.close();
+            
         }
         catch (IOException e){
           e.printStackTrace();
